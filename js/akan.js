730af573cb4f69ec32d08ac4000180886135e43d
idah <idah@moringaproject.com>
2022-10-13 20:30:10 +0300

    modified js file contents

--- js/akan.js
+++ js/akan.js
@@ -10,19 +10,18 @@ var akan = function(y, m, d ,g){
         return MALE_NAMES[d.getDay()];
     }
   }
-  //user logic:
-    console.log(g)
-  $(document).ready(function() {
-    $("form#form").submit(function(event) {
-        event.preventDefault();
-        var y = parseInt ($ ("#year").val());
-        var m = parseInt ($("#month").val());
-        var d = parseInt ($("#date").val());
-        var g = $("input[type='radio'][name='gender']:checked").val();        
-        var result =akan(y,m,d,g);
-  
-        alert("your akan name is: " + result);
-        //refresh page
-        document.getElementById("form").reset();
-    });
-  });
+
+$(document).ready(function() {
+$("form#form").submit(function(event) {
+    event.preventDefault();
+    var y = parseInt ($ ("#year").val());
+    var m = parseInt ($("#month").val());
+    var d = parseInt ($("#date").val());
+    var g = $("input[type='radio'][name='gender']:checked").val();        
+    var result =akan(y,m,d,g);
+
+    alert("your akan name is: " + result);
+    //refresh page
+    document.getElementById("form").reset();
+});
+});
