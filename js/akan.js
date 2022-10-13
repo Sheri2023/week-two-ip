d70f2e97d693a860726eeee3d0cfd3ee4fc8cf6c
idah <idah@moringaproject.com>
2022-10-13 14:30:28 +0300

    added funtion for form validation to HTML index file

--- js/akan.js
+++ js/akan.js
@@ -1,45 +1,26 @@
-var akan = function(y, m, d, g) {
-
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var d = new Date(y, --m, d);
-    
-    if (g === "Female") {
-    
-    return d && femaleNames[d.getDay()];
-    
-    } else {
-    
-    return d && maleNames[d.getDay()];
-    
-    }
-    
-    }
-    
-    $(document).ready(function() {
-    
-    $("form#form").submit(function(event) {
-    
+// development logic:
+var akan = function(y, m, d ,g){
+constfemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var days = ['Sunday', 'Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var d == new Date(y, --m, d);
+if (g === "female") {
+return d && femalenames[d.getday()];
+} else {
+    return d && malenames[d.getDay()];
+}
+}
+//user logic:
+$(document).ready(function) {
+$("form#form").submit(function(event) {
     event.preventDefault();
-    
-    var y = parseInt($("#year").val());
-    
-    var m = parseInt($("#month").val());
-    
-    var d = parseInt($("#date").val());
-    
-    var g = $("input:radio[name=gender]:checked").val();
-    
-    var result = akan(y, m, d, g);
-    
-    alert("Your akan name is: " + result);
-    
-    //refresh page
-    
-    document.getElementById("form").reset();
-    
-    });
-    
-    });
-    
+var y = parseInt ($ ("#year").val());
+var m = parseInt ($("#month").val());
+var d = parseInt ($("#date").val());
+var g = $("input:radio[name-gender]:checked").val();
+var result =akan(y,m,d,g);
+alert("your akan name is: " + result);
+//refresh page
+document.getElementById("form").reset();
+});
+});
