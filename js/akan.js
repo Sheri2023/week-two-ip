3c03c7f6ce568053e68235c1996e0cbfc74a118f
idah <idah@moringaproject.com>
2022-10-13 19:31:11 +0300

    modified js and css and index html files and added a live link to README file

--- js/akan.js
+++ js/akan.js
@@ -1,26 +1,28 @@
 // development logic:
 var akan = function(y, m, d ,g){
-constfemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var days = ['Sunday', 'Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-var d == new Date(y, --m, d);
-if (g === "female") {
-return d && femalenames[d.getday()];
-} else {
-    return d && malenames[d.getDay()];
-}
-}
-//user logic:
-$(document).ready(function) {
-$("form#form").submit(function(event) {
-    event.preventDefault();
-var y = parseInt ($ ("#year").val());
-var m = parseInt ($("#month").val());
-var d = parseInt ($("#date").val());
-var g = $("input:radio[name-gender]:checked").val();
-var result =akan(y,m,d,g);
-alert("your akan name is: " + result);
-//refresh page
-document.getElementById("form").reset();
-});
-});
+    const FEMALE_NAMES = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    const MALE_NAMES = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var days = ['Sunday', 'Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var d = new Date(y, --m, d);
+    if (g.toString().toLowerCase() === "female") {
+        return FEMALE_NAMES[d.getDay()];
+    } else {
+        return MALE_NAMES[d.getDay()];
+    }
+  }
+  //user logic:
+    console.log(g)
+  $(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var y = parseInt ($ ("#year").val());
+        var m = parseInt ($("#month").val());
+        var d = parseInt ($("#date").val());
+        var g = $("input[type='radio'][name='gender']:checked").val();        
+        var result =akan(y,m,d,g);
+  
+        alert("your akan name is: " + result);
+        //refresh page
+        document.getElementById("form").reset();
+    });
+  });
